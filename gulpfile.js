d31e3b9b6faf005d734b9b43513d14072396eac3
Finnur Sigurðsson <finnursigu@gmail.com>
2015-02-26 16:49:37 +0000

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
+var packageInfo = require('./package.json');
+
 gulp.task('default', function(){
     return gulp.src('src/VideoScroller.js')
         .pipe(uglify())
-        .pipe(rename('VideoScroller-1.0.0.min.js'))
+        .pipe(rename('VideoScroller-'+ packageInfo.version +'.min.js'))
         .pipe(gulp.dest('dist/'));
 });
